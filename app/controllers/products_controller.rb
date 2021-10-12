f755d42468655d259a6c8bd50d5f2eb37cab84d5
chetan <chetanshendge78901@gmail.com>
2021-10-12 23:34:11 +0530

    added product model

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,60 @@
+class ProductsController < ApplicationController
+  before_action :set_product, only: %i[show edit update destroy]
+
+  def index
+    @categories = Category.all
+    cate = params[:cate]
+    if !cate.nil?
+      @products = Product.where(category_id: cate)
+    else
+      @products = Product.all
+    end
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def show;  end
+
+  def edit;  end
+
+  def create
+    # byebug
+    @user = current_user.email
+    @product = User.last.products.new(product_params)
+    @product.user_id = current_user.id
+    if @product.save
+    UserMailer.new_product_email(@product, @user).deliver_now
+    redirect_to @product
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:product_name, :product_description, :product_in_strock,
+                                    :product_price, :product_size, :user_id, :category_id,
+                                    images: [])
+  end
+end
