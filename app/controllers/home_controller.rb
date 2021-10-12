96718964ec0d31f9907bcaedcaf95e50b3f37848
chetan <chetanshendge78901@gmail.com>
2021-10-12 14:05:19 +0530

    home controller added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
+
   def index
+    @products = Product.all
+    @user = User.all
+    @categories = Category.all
+    cate = params[:cate]
+
+    if !cate.nil?
+      @products = Product.where(category_id => :cate)
+    else
+      @products = Product.all
+    end
   end
 end
